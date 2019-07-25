766b85505393a513d80c20890d58205c1bafc774
Atsushi-Endo <ena-choco@live.jp>
2019-07-25 21:45:37 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,10 +2,10 @@ $(function(){
   function buildHTML(message){
       image = ( message.image ) ? `<img class='chat-content__image' src=${message.image} >` :"";
 
-    var html = `<div class='chat-content'>
+    var html = `<div class='chat-content' data-message-id="${message.id}">
                     <div class='chat-content__header'>
                       <div class='chat-content__name'>
-                        ${message.name}
+                        ${message.user_name}
                       </div>
                       <div class='chat-content__date'>
                         ${message.date}
@@ -13,9 +13,9 @@ $(function(){
                     </div>
                     <div class='chat-content__message'>
                       ${message.content}
+                      </div>
                       ${image}
-                    </div>
-                  </div>`;
+                  </div>`
     return html;
   }
 
@@ -43,4 +43,32 @@ $(function(){
       alert('メッセージの送信に失敗しました');
     })
   })
+
+  //自動更新
+
+  var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.chat-content:last').data('message-id');
+      $.ajax({
+        url: "api/messages",
+        type: 'GET',
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message){
+          insertHTML = buildHTML(message);
+          $('.chat-body').append(insertHTML);
+          $('.chat-footer__message').val('')
+          $('.hidden')  .val('')    
+          $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');    
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
