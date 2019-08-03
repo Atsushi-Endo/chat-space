4f50138ad844f06dafa4575e4eb0dcfa4de2a973
Atsushi-Endo <ena-choco@live.jp>
2019-08-03 10:16:02 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,11 +7,11 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
     @group.users << current_user
+    binding.pry
   end
 
   def create
     @group = Group.new(group_params)
-    binding.pry
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
