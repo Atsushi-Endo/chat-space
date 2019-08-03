c6f1b922e03f429d26b27c9d3b6c8f043c3da2a9
Atsushi-Endo <ena-choco@live.jp>
2019-08-03 10:09:12 +0900

    一部修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,17 +2,16 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
+    @groups = current_user.groups
   end
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
     if @group.save
-      binding.pry
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
@@ -26,6 +25,10 @@ class GroupsController < ApplicationController
       render :edit
     end
   end
+
+  def edit
+    @users = User.all
+    @group_users = @group.users
 
   private
   def group_params
