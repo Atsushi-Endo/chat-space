356a3c689c099b598ec5372a726e13dcd7887445
Atsushi-Endo <ena-choco@live.jp>
2019-08-18 21:33:36 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,11 +19,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @users = @group.users
   end
 
   def update
     if @group.update(group_params)
+      @users = @group.users
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
