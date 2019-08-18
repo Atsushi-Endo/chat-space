c5b59ced882a2d4bfbfb7027b840cbc91c5f9cae
Atsushi-Endo <ena-choco@live.jp>
2019-08-18 21:40:14 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,7 +23,6 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      @group.users << @group.users
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
@@ -32,7 +31,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])  
+    params.require(:group).permit(:name, {user_ids: []})  
   end
 
   def set_group
