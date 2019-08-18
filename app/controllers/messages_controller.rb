da3177519a2a19e384cb15b88e5fe6521f0b7556
Atsushi-Endo <ena-choco@live.jp>
2019-08-18 21:11:01 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,6 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    @members = group_member(@group)
 
     respond_to do |format|
       format.html
@@ -35,14 +34,6 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-  end
-
-  def group_member(group)
-    members = []
-    group.users.each do |member|
-      members << member.name
-    end
-    members = members.join(", ")
   end
 
 end
