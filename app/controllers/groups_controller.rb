33578524d98eb35bbbb72d4d1724c9953027ca1f
Atsushi-Endo <ena-choco@live.jp>
2019-08-19 21:41:28 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,29 +2,28 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
-    @groups = current_user.groups
   end
 
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to group_messages_path(@group.id), notice: 'グループを作成しました'
+      redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
   end
 
   def edit
-    @users = User.all
-    @group_users = @group.users
   end
 
   def update
     if @group.update(group_params)
+      @group = Group.new(group_params)
       redirect_to group_messages_path(@group.id), notice: 'グループを編集しました'
     else
       render :edit
